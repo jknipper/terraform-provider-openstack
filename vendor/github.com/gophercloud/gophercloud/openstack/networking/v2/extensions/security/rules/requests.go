e212ca248fac76a90d0ef7b38e18f6027a2d1d50
Michael Schmidt <michael.j.schmidt@gmail.com>
2018-08-29 17:57:36 +0200

    backport for outdated neutron

--- vendor/github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/security/rules/requests.go
+++ vendor/github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/security/rules/requests.go
@@ -126,6 +126,7 @@ type CreateOpts struct {
 	// TenantID is the UUID of the project who owns the Rule.
 	// Only administrative users can specify a project UUID other than their own.
 	ProjectID string `json:"project_id,omitempty"`
+	TenantID  string `json:"tenant_id,omitempty"`
 }
 
 // ToSecGroupRuleCreateMap builds a request body from CreateOpts.
